84ef11a654cc309a82cdd6632209a99fbcece39a
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-04-09 15:32:33 +0500

    notesApp done

--- NewNotesApp/script.js
+++ NewNotesApp/script.js
@@ -3,36 +3,71 @@ const addNoteBtn = document.querySelector('.addNote');
 const notesList = document.querySelector('.notesList');
 const errorMsg = document.querySelector('.errorMsg');
 
+let currentEditedValue = 0;
 addNoteBtn.addEventListener('click',()=>{
     addNote();
 })
 // creating a new note 
 function addNote(){
     const note = userInput.value.trim();
-    // console.log(note);
+    // if input is empty 
     if(note === ""){
         errorMsg.textContent = "Please write some text first!";
         return false;
     }
+    // edit functionality 
+    else if(addNoteBtn.innerText == "Edit Note"){
+        editAnyNote(currentEditedValue.target.parentElement.previousElementSibling.innerHTML);
+        currentEditedValue.target.parentElement.previousElementSibling.innerHTML = note;
+        addNoteBtn.innerText = 'Add Note';
+        userInput.value = '';
+    }
+    // adding new note 
     else{
-        createNote(note);
+        const dynamicData = createNote(note);
+        notesList.appendChild(dynamicData);
         errorMsg.textContent = "";
         userInput.value = "";
         // saving notes into localstorage  
         saveToStorage(note);
     }
-    function createNote(note){
-        const p = document.createElement('p');
-        p.textContent = note;
-        const li = document.createElement('li');
-        li.classList.add('listItem');
-        li.appendChild(p);
-        return li;       
+}
+function editAnyNote(editedNode){
+    let noteData;
+    if(localStorage.getItem('notes')=== null){
+        noteData = [];
+    }else{
+        noteData = JSON.parse(localStorage.getItem('notes'));
+        index = noteData.indexOf(editedNode);
+        noteData[index]= userInput.value;
+        noteData = localStorage.setItem('notes',JSON.stringify(noteData));
     }
+}
+function createNote(note){
+    const p = document.createElement('p');
+    p.textContent = note;
+    const li = document.createElement('li');
+    li.classList.add('listItem');
+    li.appendChild(p);
+
+    // edit button 
+    const editButton = document.createElement('button');
+    editButton.textContent = "edit";
+    editButton.classList.add('btn', 'editBtn');
+     // delete button 
+     const deleteButton = document.createElement('button');
+     deleteButton.textContent = "delete";
+     deleteButton.classList.add('btn','deleteBtn');
+        const btnDiv = document.createElement('div');
+        btnDiv.classList.add('btnDiv');
+    btnDiv.appendChild(editButton);
+    btnDiv.appendChild(deleteButton);
+    li.appendChild(btnDiv)
+    return li;       
 }
 function saveToStorage(note){
     let notesData;
-    if(localStorage.getItem('notes' === null)){
+    if(localStorage.getItem('notes') === null){
         notesData = [];
     }else{
         notesData = JSON.parse(localStorage.getItem('notes'));
@@ -40,16 +75,49 @@ function saveToStorage(note){
     notesData.push(note);
     localStorage.setItem('notes',JSON.stringify(notesData));
 }
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded',fetchAllData);
+// fetching data from local storage 
+function fetchAllData(){
     let notesData;
-    if(localStorage.getItem('notes' === null)){
+    if(localStorage.getItem('notes') === null){
         notesData = [];
     }else{
-        notesData = JSON.parse(localStorage.getItem(notes));
+        notesData = JSON.parse(localStorage.getItem("notes"));
         notesData.forEach((item)=>{
             const noteItem = createNote(item);
-
+            notesList.appendChild(noteItem);
         })
-    }
-})
+}
+}
+notesList.addEventListener('click',editAndDeleteWork);
+function editAndDeleteWork(event){
 
+    // for delete 
+    if(event.target.innerHTML === 'delete'){
+        notesList.removeChild(event.target.parentElement.parentElement);
+        // to delete from array 
+        deleteFromArray(event.target.parentElement.parentElement);
+    }
+    // for edit 
+    if(event.target.innerHTML === 'edit'){
+        let editedValue = event.target.parentElement.previousElementSibling.innerHTML;
+        userInput.value = editedValue ;
+        addNoteBtn.innerText = 'Edit Note';
+        userInput.focus();
+        currentEditedValue = event;
+    }
+}
+// deleting from localStorage  
+function deleteFromArray(currentNote){
+    let noteData;
+    if(localStorage.getItem('notes') === null){
+        noteData = [];
+    }else{
+        noteData = JSON.parse(localStorage.getItem('notes'));
+        let currentNoteItem = currentNote.firstChild.innerHTML;
+        let index = noteData.indexOf(currentNoteItem);
+        noteData.splice(index,1);
+        // sending updated array to local storage 
+        localStorage.setItem('notes',JSON.stringify(noteData));
+    }
+}
